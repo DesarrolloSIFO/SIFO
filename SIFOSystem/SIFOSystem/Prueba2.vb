63f540653bb7e4aeb371c6a1f8683d4860e2d388
oskr <hoscar161@gmail.com>
2019-03-27 23:52:34 -0600

    Diseñe formulario

--- SIFOSystem/SIFOSystem/Prueba2.vb
+++ SIFOSystem/SIFOSystem/Prueba2.vb
@@ -1,3 +1,10 @@
-﻿Public Class Prueba2
+﻿Public Class FrmFactura
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles BtnMinimizar.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.WindowState = FormWindowState.Maximized
+
+    End Sub
 End Class
