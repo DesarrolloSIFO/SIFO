c187f7344b24b480b96fd418ba427399bcd2780c
Fabiel <anthonyfabiel0@gmail.com>
2019-04-03 11:21:55 -0600

    Agreger la Conexion.vb

---
+++ SIFOSystem/SIFOSystem/Conexion/Conexion.vb
@@ -0,0 +1,5 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Module Conexion
+    Public Cn As New SqlConnection("Data Source=.\SQLEXPRESS;Initial Catalog=OpticaDb;Integrated Security=True")
+End Module
