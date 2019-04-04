0506783328fa8524d46599294eec3fa9c7372393
Oscar <hoscar161@gmail.com>
2019-04-04 14:40:33 -0600

    Programe y valide botones del formulario categoria

--- SIFOSystem/SIFOSystem/My Project/Application.Designer.vb
+++ SIFOSystem/SIFOSystem/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.SIFOSystem.FrmCiudad
+            Me.MainForm = Global.SIFOSystem.FrmCategoria
         End Sub
     End Class
 End Namespace
