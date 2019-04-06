00c65f5a48df255b8a4e8321b257b82ca0ee22be
AdrMargarita <adriortez7g@gmail.com>
2019-04-05 23:06:30 -0600

    Se genero el codigo del formulario Factura

--- SIFOSystem/SIFOSystem/Conexion/Conexion.vb
+++ SIFOSystem/SIFOSystem/Conexion/Conexion.vb
@@ -1,5 +1,5 @@
 ﻿Imports System.Data
 Imports System.Data.SqlClient
 Module Conexion
-    Public Cn As New SqlConnection("Data Source=.;Initial Catalog=OpticaDb;Integrated Security=True")
+    Public Cn As New SqlConnection("Data Source=.\SQLExpress;Initial Catalog=OpticaDb;Integrated Security=True")
 End Module
