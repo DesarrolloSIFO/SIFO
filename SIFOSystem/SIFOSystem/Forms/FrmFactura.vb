d40229cd7dea727959275660ddb63099528feee9
AdrMargarita <adriortez7g@gmail.com>
2019-04-06 21:18:04 -0600

    Factura eliminar, validado y los demas

--- SIFOSystem/SIFOSystem/Forms/FrmFactura.vb
+++ SIFOSystem/SIFOSystem/Forms/FrmFactura.vb
@@ -1,7 +1,8 @@
 ﻿Imports System.Data.SqlClient
 
 Public Class Form
-    Private Sub MostrarTodo()
+
+    Private Sub MostrarFacturas()
         If Cn.State = ConnectionState.Open Then
             Cn.Close()
         End If
@@ -22,13 +23,13 @@ Public Class Form
                 While VerFactura.Read = True
                     With LsvDetalleFac.Items.Add(VerFactura("IdFactura").ToString)
                         .SubItems.Add(VerFactura("NumIdentCliente").ToString)
-                        .SubItems.Add(VerFactura("TipoExamen").ToString)
+                        .SubItems.Add(VerFactura("IdTipoExamen").ToString)
                         .SubItems.Add(VerFactura("FechaFactura").ToString)
                         .SubItems.Add(VerFactura("IdUsuario").ToString)
                     End With
                 End While
             Catch ex As Exception
-                MessageBox.Show("Error al Mostrar los datos", "SIFO", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                MessageBox.Show("Error al Mostrar los datos" + ex.Message, "SIFO", MessageBoxButtons.OK, MessageBoxIcon.Error)
             Finally
                 Cn.Close()
             End Try
@@ -49,10 +50,9 @@ Public Class Form
                     .CommandType = CommandType.StoredProcedure
                     .Connection = Cn
 
-                    .Parameters.Add("@IdFactura", SqlDbType.Int).Value = TxtCodFactura.Text
                     .Parameters.Add("@NumIdentCliente", SqlDbType.VarChar, 15).Value = TxtNumIdentCliente.Text
                     .Parameters.Add("@IdTipoExamen", SqlDbType.Int).Value = CInt(CboTipoExamen.SelectedValue)
-                    .Parameters.Add("@FechaFactura", SqlDbType.Date).Value = DtpFechaFactura
+                    .Parameters.Add("@FechaFactura", SqlDbType.Date).Value = DtpFechaFactura.Value.Date.ToShortDateString()
                     .Parameters.Add("@IdUsuario", SqlDbType.Int).Value = CInt(CboIdUsuario.SelectedValue)
                     .ExecuteNonQuery()
 
@@ -61,14 +61,10 @@ Public Class Form
                 End With
             End Using
         Catch ex As Exception
-            MessageBox.Show("Error al insertar los datos", "SIFO", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            MessageBox.Show("Error al insertar los datos" + ex.Message, "SIFO", MessageBoxButtons.OK, MessageBoxIcon.Error)
         Finally
             Cn.Close()
         End Try
-    End Sub
-
-    Private Sub Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MostrarTodo()
     End Sub
 
     Private Sub LlenarComboUsuario()
@@ -91,7 +87,7 @@ Public Class Form
                 Da.Fill(Ds, "Usuario")
                 CboIdUsuario.DataSource = Ds.Tables(0)
 
-                CboIdUsuario.DisplayMember = Ds.Tables(0).Columns("UserName").ToString
+                CboIdUsuario.DisplayMember = Ds.Tables(0).Columns("IdUsuario").ToString
                 CboIdUsuario.ValueMember = Ds.Tables(0).Columns("IdUsuario").ToString
 
             Catch ex As Exception
@@ -127,7 +123,7 @@ Public Class Form
                 CboTipoExamen.ValueMember = Ds.Tables(0).Columns("IdTipoExamen").ToString
 
             Catch ex As Exception
-                MessageBox.Show("Error al Mostrar los datos", "SIFO", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                MessageBox.Show("Error al Mostrar los datos TipoExamen" + ex.Message, "SIFO", MessageBoxButtons.OK, MessageBoxIcon.Error)
             Finally
                 Cn.Close()
             End Try
@@ -153,16 +149,18 @@ Public Class Form
     End Sub
 
     Private Sub BtnGuardar_Click(sender As Object, e As EventArgs) Handles BtnGuardar.Click
-        HabilitarBotones(True, False, False, False, True)
-        GuardarFactura()
-        MostrarTodo()
+        If ValidarTextBox() = True Then
+            HabilitarBotones(True, False, False, True, True)
+            GuardarFactura()
+            MostrarFacturas()
+        End If
     End Sub
-
 
     Private Sub BtnCancelar_Click(sender As Object, e As EventArgs) Handles BtnCancelar.Click
         HabilitarBotones(True, False, False, False, True)
         Limpiar()
     End Sub
+
     Private Sub Limpiar()
         TxtNumIdentCliente.Clear()
         TxtCodFactura.Clear()
@@ -179,14 +177,14 @@ Public Class Form
             Cn.Open()
             Using Cmd As New SqlCommand
                 With Cmd
-                    .CommandText = "Sp_ModificarFactura"
+                    .CommandText = "Sp_ActualizarFactura"
                     .CommandType = CommandType.StoredProcedure
                     .Connection = Cn
 
                     .Parameters.Add("@IdFactura", SqlDbType.Int).Value = TxtCodFactura.Text
                     .Parameters.Add("@NumIdentCliente", SqlDbType.VarChar, 15).Value = TxtNumIdentCliente.Text
                     .Parameters.Add("@IdTipoExamen", SqlDbType.Int).Value = CInt(CboTipoExamen.SelectedValue)
-                    .Parameters.Add("@FechaFactura", SqlDbType.Date).Value = DtpFechaFactura
+                    .Parameters.Add("@FechaFactura", SqlDbType.Date).Value = DtpFechaFactura.Value.Date.ToShortDateString()
                     .Parameters.Add("@IdUsuario", SqlDbType.Int).Value = CInt(CboIdUsuario.SelectedValue)
                     .ExecuteNonQuery()
 
@@ -201,11 +199,90 @@ Public Class Form
         End Try
     End Sub
 
-    Private Sub BtnModificar_Click(sender As Object, e As EventArgs) Handles BtnModificar.Click
-        ActualizarFactura()
-    End Sub
-
     Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles BtnSalir.Click
         Close()
     End Sub
+
+    Private Sub Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MostrarFacturas()
+    End Sub
+
+    Private Sub EliminarFactura()
+        If Cn.State = ConnectionState.Open Then
+            Cn.Close()
+        End If
+
+        Try
+            Cn.Open()
+            Using Cmd As New SqlCommand
+                With Cmd
+                    .CommandText = "Sp_EliminarFactura"
+                    .CommandType = CommandType.StoredProcedure
+                    .Connection = Cn
+
+
+                    Dim Id As Integer
+                    Id = CInt(LsvDetalleFac.FocusedItem.SubItems(0).Text)
+                    .Parameters.Add("@IdFactura", SqlDbType.Int).Value = Id
+                    .ExecuteNonQuery()
+
+                    MessageBox.Show("Registro Eliminado Satisfactoriamente", "SIFO", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+                End With
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error al elimnar la Factura", "SIFO", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            Cn.Close()
+        End Try
+    End Sub
+
+    Private Sub EditarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EditarToolStripMenuItem.Click
+        HabilitarBotones(False, False, True, True, True)
+        TxtCodFactura.Text = LsvDetalleFac.FocusedItem.SubItems(0).Text
+        TxtNumIdentCliente.Text = LsvDetalleFac.FocusedItem.SubItems(1).Text
+        CboTipoExamen.Text = LsvDetalleFac.FocusedItem.SubItems(2).Text
+        DtpFechaFactura.Value = LsvDetalleFac.FocusedItem.SubItems(3).Text
+        CboIdUsuario.Text = LsvDetalleFac.FocusedItem.SubItems(4).Text
+
+        TpMenuFact.SelectedIndex = 0
+    End Sub
+
+    Private Sub BtnModificar_Click(sender As Object, e As EventArgs) Handles BtnModificar.Click
+        HabilitarBotones(False, False, True, True, True)
+        ActualizarFactura()
+        MostrarFacturas()
+
+        TpMenuFact.SelectedIndex = 1
+    End Sub
+
+    Private Sub EliminarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EliminarToolStripMenuItem.Click
+        EliminarFactura()
+        MostrarFacturas()
+    End Sub
+
+    Private Function ValidarTextBox()
+        Dim Estado As Boolean
+
+        If TxtNumIdentCliente.Text = Nothing And CboIdUsuario.SelectedValue = -1 And CboTipoExamen.SelectedValue = -1 Then
+            EpMensajeFact.SetError(TxtNumIdentCliente, "Ingrese el numero de identidad del cliente")
+            EpMensajeFact.SetError(CboIdUsuario, "Seleccione el usuario")
+            EpMensajeFact.SetError(CboTipoExamen, "Seleccione el Tipo de Examen")
+            Estado = False
+        ElseIf TxtNumIdentCliente.Text = Nothing Then
+            EpMensajeFact.SetError(TxtNumIdentCliente, "Ingrese el numero de identidad del cliente")
+            TxtNumIdentCliente.BackColor = Color.LightBlue
+            TxtNumIdentCliente.Focus()
+            Estado = False
+        ElseIf CboIdUsuario.SelectedValue = -1 Then
+            EpMensajeFact.SetError(CboIdUsuario, "Seleccione el usuario")
+            Estado = False
+        ElseIf CboTipoExamen.SelectedValue = -1 Then
+            EpMensajeFact.SetError(CboTipoExamen, "Seleccione el Tipo de Examen")
+            Estado = False
+        Else
+            Estado = True
+        End If
+        Return Estado
+    End Function
 End Class
